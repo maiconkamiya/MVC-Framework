f8d9a9060e85a5d8d753eb304563a0cf05f1593c
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2021-10-18 10:31:47 -0400

    pull init

---
+++ src/controller/homeController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maico
+ * Date: 09/10/2021
+ * Time: 11:24
+ */
+
+namespace criativa\controller;
+
+use criativa\lib\Controller;
+
+class homeController extends Controller {
+    public function index(){
+        $this->dados = array(
+            'area' => $this->getArea(),
+            'controller' => $this->getController(),
+            'action' => $this->getAction(),
+            'params' => array(
+                'primeiro'=>$this->getParams(0),
+                'segundo'=>$this->getParams(1),
+                'terceiro'=>$this->getParams(2)
+            )
+        );
+        $this->view();
+    }
+}
---
+++ src/helper/String.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maico
+ * Date: 11/10/2021
+ * Time: 10:24
+ */
+
+function _clearString(){
+
+}
