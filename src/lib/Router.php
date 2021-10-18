c8011f41363b65b083f44e6c0c233a6e3229a994
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2021-10-18 08:34:21 -0400

    Update Router.php
    mod Criativa

--- src/lib/Router.php
+++ src/lib/Router.php
@@ -4,14 +4,18 @@ namespace criativa\lib;
 
 class Router {
 
-    protected static $routers;
+    protected static $routers = array(
+        'criativa' => 'criativa'
+    );
 
     protected static $routerOnDefault = 'web';
 
     protected static $onDefault = true;
 
+    protected static $modCriativa = false;
+
     public static function setRouters($router){
-        self::$routers = $router;
+        self::$routers[] = $router;
     }
 
     public static function setRouterOnDefault($router){
