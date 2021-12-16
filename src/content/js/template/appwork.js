0a8cc0d5488c8392533306eb654d9969498dd7fe
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2021-12-16 12:00:43 -0400

    try model

--- src/content/js/template/appwork.js
+++ src/content/js/template/appwork.js
@@ -1,6 +1,3 @@
-/**
- * Created by maico on 25/10/2021.
- */
 var template = {
     'rotina': {
         'item': '<li class="sidenav-item"><a href="[uri]" [attr] class="sidenav-link [class]"><i class="sidenav-icon [icon]"></i><div>[nome]</div></a>[sub]</li>',
