69dbe132c5333ac92062a88b902d566a4ed634a0
Maicon Takeshi Kamiya <maiconkamiya@hotmail.com>
2024-06-13 08:55:46 -0400

    init

--- src/lib/System.php
+++ src/lib/System.php
@@ -155,7 +155,8 @@ class System extends Router {
     if (Router::$modCriativa){
         $this->init = str_replace('/','\\',$this->area) . '\\controller\\' . $this->controller . 'Controller';
     } else {
-        $this->init = 'mvc\\controller\\' . $this->area . '\\' . $this->controller . 'Controller';
+        //$this->init = 'mvc\\controller\\' . $this->area . '\\' . $this->controller . 'Controller';
+        $this->init = 'controller\\' . $this->area . '\\' . $this->controller . 'Controller';
     }
 
     $this->_validarController();
